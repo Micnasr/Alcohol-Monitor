8837851de480e1c1bbc34c6a0dc215667e86845c
Rahim Aziz <rahimaziz31@gmail.com>
2023-01-14 23:51:40 -0500

    Navbar css

--- lcoholmonitor/src/App.js
+++ lcoholmonitor/src/App.js
@@ -4,7 +4,17 @@ import Items from './components/Items'
 function App() {
   return (
     <>
-      <div>Alcohol Monitor</div>
+    <div>
+        <ul class = "navbar">
+        <li ><a class="active" id="first-nav" href=""><img class="navbar-icon" src="https://img.icons8.com/color/96/null/beer-glass.png"/></a></li>
+        <li><a href="https://www.linkedin.com/in/michaelnasr03/">Michael Nasr</a></li>
+        <li><a href="https://www.linkedin.com/in/qasim-s-b864b61a9/">Qasim Saadat</a></li>
+        <li><a href="https://www.linkedin.com/in/rahim-aziz/">Rahim Aziz</a></li>
+        <div class="topnav-right">
+            <li><a href="https://www.google.com/maps/">Google Maps</a></li>
+        </div>
+        </ul>      
+      </div>
       <Items />
     </>
   );
