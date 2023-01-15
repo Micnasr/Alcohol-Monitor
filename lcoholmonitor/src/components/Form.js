78062f1dfe0848dd0242ea21c016ed5cc52aa02e
Qasim Saadat <saadatq@mcmaster.ca>
2023-01-15 07:08:22 -0500

    new drinks

--- lcoholmonitor/src/components/Form.js
+++ lcoholmonitor/src/components/Form.js
@@ -57,6 +57,20 @@ const Form = props => {
                         <option value="Budweiser-5">Budweiser</option>
                         <option value="Bud Light-4.2">Bud Light</option>
                         <option value="Bacardi-40">Bacardi</option>
+                        <option value="Smirnoff-37.5">Smirnoff Red Label</option>
+                        <option value="Hennessy-40">Hennessy</option>
+                        <option value="Absolut-50">Absolut</option>
+                        <option value="Guinness-4.2">Guinness</option>
+                        <option value="Twisted Tea-5">Twisted Tea</option>
+                        <option value="Margarita-20">Margarita</option>
+                        <option value="Tequila-40">Tequila</option>
+                        <option value="Bloody Mary-10">Bloody Mary</option>
+                        <option value="Jack Daniels-40">Jack Daniels</option>
+                        <option value="Martini-28.7">Martini</option>
+                        <option value="Mojito-13">Mojito</option>
+                        <option value="Jameson-40">Jameson</option>
+                        <option value="Arak-53">Arak</option>
+                        <option value="Water-0">Water</option>
                         </select>
                         </div>
 
