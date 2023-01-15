39a896dcbcc5bc2eeb8ffc258fccff2998c16e83
Rahim Aziz <rahimaziz31@gmail.com>
2023-01-15 06:37:20 -0500

    Navbar title

--- lcoholmonitor/src/App.js
+++ lcoholmonitor/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div>
         <ul className = "navbar">
         <li ><a className="active" id="first-nav" href=""><img className="navbar-icon" src="https://img.icons8.com/color/96/null/beer-glass.png"/></a></li>
-        <li><a>Is it safe to drive?</a></li>
+        <li><a>Alcohol Monitor</a></li>
           <div className="topnav-right">
             <li><a href="https://www.linkedin.com/in/michaelnasr03/">Michael Nasr</a></li>
             <li><a href="https://www.linkedin.com/in/qasim-s-b864b61a9/">Qasim Saadat</a></li>
